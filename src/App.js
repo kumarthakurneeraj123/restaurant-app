6f2d593e6e4eb1235c774c2f1f513225180013d6
kumarthakurneeraj123 <100923765+kumarthakurneeraj123@users.noreply.github.com>
2022-04-08 23:22:05 +0530

    Meals Summary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, {Fragment} from 'react';
 
+import Meals from './components/Meals/Meals';
 
 import Header from './components/Layout/Header';
 function App() {
   return (
     <Fragment>
         <Header/>
+        <main>
+          <Meals />
+        </main>
     </Fragment>
   );
 }
