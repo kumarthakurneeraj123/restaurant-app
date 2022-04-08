293cfe458cf18caeecfda9c012f9fb8c1521a03a
kumarthakurneeraj123 <100923765+kumarthakurneeraj123@users.noreply.github.com>
2022-04-08 23:54:43 +0530

    Making Item Visible

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Fragment} from 'react';
+import classes from './App.module.css';
 
 import Meals from './components/Meals/Meals';
 
 import Header from './components/Layout/Header';
 function App() {
   return (
-    <Fragment>
+    <Fragment className={classes.fragment}>
         <Header/>
         <main>
           <Meals />
