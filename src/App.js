dcdb950ecd9ce407eebfd740cbd67d70c00c8da6
kumarthakurneeraj123 <100923765+kumarthakurneeraj123@users.noreply.github.com>
2022-04-09 14:46:22 +0530

    Cart open close

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 
 import classes from './App.module.css';
 
@@ -6,10 +7,17 @@ import Cart from './components/Cart/Cart';
 
 import Header from './components/Layout/Header';
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const showCartHandler =()=>{
+    setCartIsShown(true);
+  }
+  const hideCartHandler =()=>{
+    setCartIsShown(false);
+  }
   return (
     <div className={classes.fragment}>
-        <Cart />
-        <Header/>
+        {cartIsShown && <Cart onClose={hideCartHandler}/>}
+        <Header onShowCart={showCartHandler}/>
         <main>
           <Meals />
         </main>
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -3,9 +3,9 @@ import React from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css';
 
-const HeaderCartButton =()=>{
+const HeaderCartButton =(props)=>{
     return (
-        <button className={classes.button}>
+        <button onClick={props.onShowCart} className={classes.button}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
