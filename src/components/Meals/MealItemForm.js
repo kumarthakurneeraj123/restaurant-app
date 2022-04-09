becfb253765f339bfc3656f778ccdab9614b6ff2
kumarthakurneeraj123 <100923765+kumarthakurneeraj123@users.noreply.github.com>
2022-04-09 17:52:10 +0530

    show cart items

--- src/components/Meals/MealItemForm.js
+++ src/components/Meals/MealItemForm.js
@@ -1,10 +1,25 @@
+import { useRef , useState} from 'react';
 
 import classes from './MealItemForm.module.css';
 import Input from '../UI/Input';
 
 const MealItemForm =(props)=>{
-    return <form className={classes.form}>
-        <Input label="Amount" input ={{
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const inputAmountRef = useRef();
+    const submitHandler = (event)=>{
+        event.preventDefault();
+        const enteredAmount = inputAmountRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+
+        if(enteredAmount.trim().length===0 || enteredAmountNumber<1 || enteredAmountNumber>5){
+            setAmountIsValid(false);
+            return;
+        }
+        props.onAddToCart(enteredAmountNumber);
+    };
+
+    return <form onSubmit={submitHandler} className={classes.form}>
+        <Input ref={inputAmountRef} label="Amount" input ={{
             id:"Amount",
             type:"number",
             min:"1",
@@ -13,6 +28,7 @@ const MealItemForm =(props)=>{
             defaultValue:'1'
         }}/>
         <button>Add</button>
+        {!amountIsValid && <p>Please enter valid input (1-5).</p>}
     </form>
 }
 export default MealItemForm;
